d47e1d4a37f13c6c8320d61a377057eba6a04ee9
chi <chim20142019@gmail.com>
2020-03-16 14:48:44 -0400

    using LoginComponent in the MainActivity.ie, Using the component by the client

--- app/src/main/java/com/example/daggerpracticeeg1/MainActivity.kt
+++ app/src/main/java/com/example/daggerpracticeeg1/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.daggerpracticeeg1
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.daggerpracticeeg1.di.component.DaggerLoginComponent
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val loginComponent = DaggerLoginComponent.create()
+        loginComponent.getLoginManager().login("abc", "123")
     }
 }
